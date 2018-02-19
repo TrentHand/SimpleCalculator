029c3d538da100b8568ef1a35084821f1ef1c268
Trent Hand <livefree1983@gmail.com>
2018-02-19 17:09:38 -0600

    added a try/catch to handle any exceptions

--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -10,15 +10,23 @@ namespace SimpleCalculator
     {
         static void Main(string[] args)
         {
-            InputConverter inputConverter = new InputConverter();
-            CalculatorEngine calculatorEngine = new CalculatorEngine();
+            try
+            {
+                InputConverter inputConverter = new InputConverter();
+                CalculatorEngine calculatorEngine = new CalculatorEngine();
 
-            double firstNumber = inputConverter.ConvertInputToNumberic(Console.ReadLine());
-            double secondNumber = inputConverter.ConvertInputToNumberic(Console.ReadLine());
-            string operation = Console.ReadLine();
+                double firstNumber = inputConverter.ConvertInputToNumberic(Console.ReadLine());
+                double secondNumber = inputConverter.ConvertInputToNumberic(Console.ReadLine());
+                string operation = Console.ReadLine();
 
-            double result = calculatorEngine.Calculate(operation, firstNumber, secondNumber);
-            Console.WriteLine(result);
+                double result = calculatorEngine.Calculate(operation, firstNumber, secondNumber);
+                Console.WriteLine(result);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
         }
     }
 }
